3868db3888c650e3f7ca9a0a4eaca6c566a69f5f
davis-mwangi <daudimwash@gmail.com>
2018-10-07 00:55:50 +0300

    Updates

---
+++ precache-manifest.4c73406f341feb343e3e12083ed9cd89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a87b797d414c38be7a7b072c28453e6",
+    "url": "/react-gh-pages/static/media/logo-wb-header-en.8a87b797.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "bff97ae6c49206e3cbf3",
+    "url": "/react-gh-pages/static/js/main.bff97ae6.chunk.js"
+  },
+  {
+    "revision": "700876ddf8b1be926fea",
+    "url": "/react-gh-pages/static/js/1.700876dd.chunk.js"
+  },
+  {
+    "revision": "bff97ae6c49206e3cbf3",
+    "url": "/react-gh-pages/static/css/main.73dea9e8.chunk.css"
+  },
+  {
+    "revision": "3ded71883a231b1db1b6d1788192d614",
+    "url": "/react-gh-pages/index.html"
+  }
+];
